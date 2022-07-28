25e40cbdebe00e94fbcd1757152c073db5dcdd14
Ambrose Chua <ambrose@hea.care>
2022-07-28 17:22:24 +0800

    Remove old CI

--- tool/ci.sh
+++ tool/ci.sh
@@ -68,16 +68,16 @@ for PKG in ${PKGS}; do
       echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
       case ${TASK} in
       analyze)
-        echo 'dart analyze'
-        dart analyze || EXIT_CODE=$?
+        echo 'dart analyze | sed 's/^   info/warning/g''
+        dart analyze | sed 's/^   info/warning/g' || EXIT_CODE=$?
         ;;
       format)
         echo 'dart format --output=none --set-exit-if-changed .'
         dart format --output=none --set-exit-if-changed . || EXIT_CODE=$?
         ;;
       test)
-        echo 'dart test'
-        dart test || EXIT_CODE=$?
+        echo 'dart test --test-randomize-ordering-seed=random'
+        dart test --test-randomize-ordering-seed=random || EXIT_CODE=$?
         ;;
       *)
         echo -e "\033[31mUnknown TASK '${TASK}' - TERMINATING JOB\033[0m"
